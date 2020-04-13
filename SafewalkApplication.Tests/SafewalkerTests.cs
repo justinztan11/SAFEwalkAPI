a28a09c7249c63c4a83c1aae255f3c3c47ab732c
Yoon Cho <yoon172@gmail.com>
2020-04-13 00:57:14 -0500

    User and Safewalker tests updated

--- SafewalkApplication.Tests/SafewalkerTests.cs
+++ SafewalkApplication.Tests/SafewalkerTests.cs
@@ -20,10 +20,26 @@ namespace SafewalkApplication.Tests
             _client = server.CreateClient();
         }
 
+        const string SafeWalker11Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImY3N2JlMDk4LTI0YWEtNDQxNi1iNzc1LTY4OGU3YjUzYzk4ZSIsIm5iZiI6MTU4Njc1NDM4NSwiZXhwIjoxNTg2ODQwNzg1LCJpYXQiOjE1ODY3NTQzODV9.J5Zv3EYgH3K9oJMmg7hORrHRHNLCxjLF2t3bRCaJHcc";
+        const string SafeWalker12Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjFhMWViMGFjLTgxNTEtNGIzYi1hZmVkLTc3NWRjOTYzYzY3ZiIsIm5iZiI6MTU4Njc1NDQ3NSwiZXhwIjoxNTg2ODQwODc1LCJpYXQiOjE1ODY3NTQ0NzV9.3Mq-CJ8kl6CviA4jHiTRYBFynNCZUNt1kri1uCb0ov0";
+        const string SafeWalker13Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImZiODA2ZWI0LWE1YmQtNDk1Mi1iZWIzLWIyYzdjZTU4MGE4MyIsIm5iZiI6MTU4Njc1NDQ4NiwiZXhwIjoxNTg2ODQwODg2LCJpYXQiOjE1ODY3NTQ0ODZ9.wEXi8BTwkK9d11_MxUFG8-tYuc_6bdTNDiVQ1YnuFho";
+        const string SafeWalker14Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjdiZTJjNTNjLTczOGItNDEwZC1hNzg1LWY5YjYxNmJiZjk4MyIsIm5iZiI6MTU4Njc1NDQ5NywiZXhwIjoxNTg2ODQwODk3LCJpYXQiOjE1ODY3NTQ0OTd9.B6hatRrCdmlOBkVLqndJ1S8_JtibUzb8PtgD1CmYmu4";
+        const string SafeWalker15Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImVjMzI4NDlhLTEyMzYtNDU3Mi1hODcyLTIwYWYwZWM2ZWIxMSIsIm5iZiI6MTU4Njc1NDU3MCwiZXhwIjoxNTg2ODQwOTcwLCJpYXQiOjE1ODY3NTQ1NzB9.YBy8jksddy67-7CauhFkzNvatIAK5f1ypOD0UEFYtcQ";
+
+        const string SafeWalker11Email = "walkertest11@wisc.edu";
+        const string SafeWalker12Email = "walkertest12@wisc.edu";
+        const string SafeWalker13Email = "walkertest13@wisc.edu";
+        const string SafeWalker14Email = "walkertest14@wisc.edu";
+        const string SafeWalker15Email = "walkertest15@wisc.edu";
+
+        const string SafeWalkerPassword = "test11";
+
         // GetSafewalker Tests ----------------------------------------------------------------------
 
         [TestMethod]
-        [DataRow("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEyMzQ1Njc4OTg3NjVoYyIsIm5iZiI6MTU4Mzc2NTE1MCwiZXhwIjoxNTgzODUxNTUwLCJpYXQiOjE1ODM3NjUxNTB9.lIqN2RuvbOK79Succ98r3DnlDa59MfahHddfNMyArsA", "shimura@wisc.edu", "false")]
+        [DataRow(SafeWalker11Token, SafeWalker11Email, "false")]
+        [DataRow(SafeWalker12Token, SafeWalker12Email, "false")]
+        [DataRow(SafeWalker13Token, SafeWalker13Email, "false")]
         public void GetSafewalker_Ok(string token, string email, string isUser)
         {
             //Arrange
@@ -39,7 +55,9 @@ namespace SafewalkApplication.Tests
         }
 
         [TestMethod]
-        [DataRow("dummytoken", "shimura@wisc.edu", "false")]
+        [DataRow("dummytoken", SafeWalker11Email, "false")]
+        [DataRow(SafeWalker12Token, "shimura@wisc.edu", "false")]
+        [DataRow("dummytoken", "dummyemail@wisc.edu", "false")]
         public void GetSafewalker_Unauthorized(string token, string email, string isUser)
         {
             //Arrange
@@ -56,8 +74,8 @@ namespace SafewalkApplication.Tests
 
         // PutSafewalker Tests ----------------------------------------------------------------------
         [TestMethod]
-        [DataRow("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjM5MTg0MzkxLTczZDEtNGJjOC05NjQ3LTlkYmVmZjcxMTAyYiIsIm5iZiI6MTU4Mzc2Mzg1MiwiZXhwIjoxNTgzODUwMjUyLCJpYXQiOjE1ODM3NjM4NTJ9.ihcQU9HSMgs78yCaWs4RRV4Fkx1_Bsj2C2EZNi-9cjE", "fakedeuman@wisc.edu", "{\"LastName\":\"Deuman\", \"FirstName\":\"Freakin\", \"Password\":\"crap11\", \"PhoneNumber\":\"4567890\", \"HomeAddress\":\"lakeshore\"}")]
-        [DataRow("dummytoken", "ycho@wisc.edu", "{\"LastName\":\"Cho\", \"FirstName\":\"Yoon\", \"Password\":\"cat11\", \"PhoneNumber\":\"8342532244\", \"HomeAddress\":\"9384 street\"}")]
+        [DataRow(SafeWalker14Token, "fakedeuman@wisc.edu", "{\"LastName\":\"Deuman\", \"FirstName\":\"Freakin\", \"Password\":\"crap11\", \"PhoneNumber\":\"4567890\", \"HomeAddress\":\"lakeshore\"}")]
+        [DataRow("dummytoken", SafeWalker14Email, "{\"LastName\":\"Cho\", \"FirstName\":\"Yoon\", \"Password\":\"cat11\", \"PhoneNumber\":\"8342532244\", \"HomeAddress\":\"9384 street\"}")]
         public void PutSafewalker_Unauthorized(string token, string email, string user)
         {
             //Arrange
@@ -74,7 +92,7 @@ namespace SafewalkApplication.Tests
         }
 
         [TestMethod]
-        [DataRow("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEyMzQ1Njc4OTg3NjVoYyIsIm5iZiI6MTU4Mzc2NTE1MCwiZXhwIjoxNTgzODUxNTUwLCJpYXQiOjE1ODM3NjUxNTB9.lIqN2RuvbOK79Succ98r3DnlDa59MfahHddfNMyArsA", "shimura@wisc.edu", "{\"LastName\":\"Shimura\", \"FirstName\":\"Tadao\", \"Password\":\"shimura11\", \"PhoneNumber\":\"9824323438\"}")]
+        [DataRow(SafeWalker15Token, SafeWalker15Email, "{\"LastName\":\"Shimura\", \"FirstName\":\"Tadao\", \"Password\":\"shimura11\", \"PhoneNumber\":\"9824323438\"}")]
         public void PutSafewalker_Ok(string token, string email, string user)
         {
             //Arrange
